d6354a77ac9cc3ee4111bf2b686da8fabc3801a6
KazuhaaX <yuikaa124@gmail.com>
2025-04-02 11:17:04 +0700

    Update Question.java

--- src/models/Question.java
+++ src/models/Question.java
@@ -1,1 +1,23 @@
+public class Question {
+    private final int number;
+    private final String difficulty;
+    private final String text;
+    private final String[] options;
+    private final char correctAnswer;
 
+    public Question(int number, String difficulty, String text, 
+                   String[] options, char correctAnswer) {
+        this.number = number;
+        this.difficulty = difficulty;
+        this.text = text;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+
+    // Getters
+    public int getNumber() { return number; }
+    public String getDifficulty() { return difficulty; }
+    public String getText() { return text; }
+    public String[] getOptions() { return options; }
+    public char getCorrectAnswer() { return correctAnswer; }
+}
